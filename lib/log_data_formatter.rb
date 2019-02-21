9e05675e39282cd4b3be506fa3be75de2e647145
Reka <rekapap28@gmail.com>
2019-02-21 17:51:05 +0000

    adds log_data_formatter#format_unique

--- lib/log_data_formatter.rb
+++ lib/log_data_formatter.rb
@@ -3,4 +3,7 @@ class LogDataFormatter
   def self.format_total(visits)
     visits.map { |uri, count| "#{uri} #{count} visits." }.join("\n")
   end
+  def self.format_unique(visits)
+    visits.map { |uri, count| "#{uri} #{count} unique visitors." }.join("\n")
+  end
 end
--- spec/log_data_formatter_spec.rb
+++ spec/log_data_formatter_spec.rb
@@ -4,9 +4,15 @@ describe LogDataFormatter do
   let(:described_class) { LogDataFormatter }
   let(:data) { [['uri', 2],['uri2', 1]] }
   let(:total_visits) { "uri 2 visits.\nuri2 1 visits." }
+  let(:unique_visits) { "uri 2 unique visitors.\nuri2 1 unique visitors." }
   describe '.format_total' do
     it 'returns a string with the date formatted like this: {uri} {count} visits' do
       expect(described_class.format_total(data)).to eq(total_visits)
+    end
+  end
+  describe '.format_unique' do
+    it 'returns a string with the date formatted liek this: {uri} {count} visits' do
+      expect(described_class.format_unique(data)).to eq(unique_visits)
     end
   end
 end
