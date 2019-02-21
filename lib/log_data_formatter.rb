d41b45b00b1b9e1dae9f2e38146c0061d5da9f1e
Reka <rekapap28@gmail.com>
2019-02-21 18:13:32 +0000

    adds comments for log_data_formatter

--- lib/log_data_formatter.rb
+++ lib/log_data_formatter.rb
@@ -2,10 +2,12 @@
 
 # Formatter
 class LogDataFormatter
+  # Returns a string in the total visit format from an array of uris and counts
   def self.format_total(visits)
     visits.map { |uri, count| "#{uri} #{count} visits." }.join("\n")
   end
 
+  # Returns a string in the unique visit format from an array of uri and counts
   def self.format_unique(visits)
     visits.map { |uri, count| "#{uri} #{count} unique visitors." }.join("\n")
   end
